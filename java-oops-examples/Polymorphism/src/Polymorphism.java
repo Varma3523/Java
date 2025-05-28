f88cc3d10c4da9c7e65eba130d9e8326011bc18c
Yashwanth <yashwanthvarma3523@gmail.com>
2025-05-28 15:23:50 -0400

    Polymorphism

---
+++ java-oops-examples/Polymorphism/src/Polymorphism.java
@@ -0,0 +1,38 @@
+
+
+
+//         Polymorphism allows objects of different classes to be treated as objects of a common superclass.
+
+
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.sound();  // Animal makes a sound
+        myDog.sound();     // Dog barks
+        myCat.sound();     // Cat meows
+    }
+}
