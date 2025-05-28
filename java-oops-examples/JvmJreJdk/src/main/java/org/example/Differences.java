2dbc77a0bd61e3242c4282a64d6eb81e15b7e393
Yashwanth <yashwanthvarma3523@gmail.com>
2025-05-28 15:06:49 -0400

    Differences btwn JRE JDK JVM

---
+++ java-oops-examples/JvmJreJdk/src/main/java/org/example/Differences.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+/*                    JVM (Java Virtual Machine): Executes Java bytecode (platform dependent)
+                      JRE (Java Runtime Environment): JVM + libraries needed to run Java programs
+                      JDK (Java Development Kit): JRE + tools needed to develop Java programs (compiler, debugger, etc.)
+
+
+
+                      */
