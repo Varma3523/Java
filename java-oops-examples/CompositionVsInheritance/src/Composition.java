54f72219cbb0c2745f3b08c72610a44336d590cc
Yashwanth <yashwanthvarma3523@gmail.com>
2025-05-28 15:28:18 -0400

    Composition vs Inheritance

---
+++ java-oops-examples/CompositionVsInheritance/src/Composition.java
@@ -0,0 +1,27 @@
+
+
+
+//                                       Composition (has-a relationship)
+
+
+
+
+/* class Engine {
+    void start() { System.out.println("Engine started"); }
+   }
+
+class Car {
+    private Engine engine;  // Car has an Engine
+
+    Car() {
+        this.engine = new Engine();
+    }
+
+    void start() {
+        engine.start();
+    }
+}
+
+
+
+*/
---
+++ java-oops-examples/CompositionVsInheritance/src/Inheritance.java
@@ -0,0 +1,21 @@
+
+
+
+//               Inheritance (is-a relationship):
+
+
+
+
+
+
+/*
+
+class Vehicle {
+    void move() { System.out.println("Moving"); }
+}
+
+class Car extends Vehicle {
+    // Car is a Vehicle
+}
+
+*/
