b04113440f386650dee672262e6fb27957033d03
Yashwanth <yashwanthvarma3523@gmail.com>
2025-06-02 14:24:44 -0400

    Autowiring with @Autowired

---
+++ spring-boot-examples/autowiring/src/main/java/org/example/UserService.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/*
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired // Constructor injection (recommended)
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Field injection (not recommended)
+    // @Autowired
+    // private UserRepository userRepository;
+
+    // Setter injection
+    // @Autowired
+    // public void setUserRepository(UserRepository userRepository) {
+    //     this.userRepository = userRepository;
+    // }
+}
+
+ */
